001890300ea8957654ca1e679855bf43b9f1ef32
quantumduck <quantum.duck@hotmail.com>
2017-04-01 10:47:21 -0400

    working on main menu display

--- ui.rb
+++ ui.rb
@@ -1,31 +1,64 @@
+require 'io/console'
+
 class UI
 
-  attr_accessor :width, :height
+  attr_reader :max_width, :max_height
 
-  def initialize(width, height)
-    @width = width  #in characters
-    @height = height #in lines
+  def initialize
+    @max_height, @max_width = $stdin.winsize
+    # puts "Your screen is #{max_width} wide and #{max_height} tall"
   end
 
-  def print_menu(title, items)
-    puts
-    puts
-    puts "**********************************************"
-    puts "* Welcome to the Customer Relations Manager! *"
-    puts "**********************************************"
-    puts "* ===============MAIN MENU================== *"
-    puts "* [B]rowse contacts                          *"
-    puts "* [A]dd new contact                          *"
-    puts "* [F]ind contact                             *"
-    puts "* [S]eacth contacts                          *"
-    puts "**********************************************"
-    puts
-    puts
+  def print_menu(title, items, start_at = 0)
+    # Centre the title
+    padding = title.to_s.length < (max_width - 6)
+    if (padding > 0)
+      padleft = padding / 2
+      padright = padding - padleft
+      title = (' ' * padleft) + title + (' ' * padright)
+    end
+
+    # if starting at higher index, indicate scrolling option:
+    if (start_at > 0)
+      items = items[start_at, items.length - start_at]
+      puts "** ^^" + ('*' * (max_width - 8)) + "^^ **"
+    print_menu_item(title)
+    puts '*' * max_width
+    # Print as many items as will fit on screen.
+    lines_left = ((max_height - 5) - items.to_a.length)
+    if lines_left >= 1
+      # Print the whole menu + bottom border and padding
+      items.each { |item| print_menu_item(item) }
+      puts '*' * max_width
+      lines_left.times { puts }
+    elsif lines left == 0
+      # Print the whole menu + bottom border
+      items.each { |item| print_menu_item(item) }
+      puts '*' * max_width
+    else
+      lines_left.times
+    end
+
+
   end
 
 
-  def print_menu_item
-
+  def print_menu_item(item, pfix = nil)
+    line =
+    case pfix.class
+    when Integer
+      line = "** " + pfix.to_s + ": " + item.to_s
+    when String
+      line = "** " + pfix + item.to_s
+    else
+      line = "** " + item.to_s
+    end
+    if (line.length > (max_width - 3))
+      line = line.to_s[0, max_width - 3] + "..."
+    else
+      line = line.to_s + (" " * (max_width - line.length - 3)) + " **"
+    end
+    puts line
   end
 
   def get_hash(keys)
